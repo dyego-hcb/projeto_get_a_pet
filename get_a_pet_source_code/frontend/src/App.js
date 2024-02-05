f46ab3cc478ca9528eafc9b5c7854f8efcaae600
Dyego Henrique Costa Braga <dyego.hcb@gmail.com>
2024-02-05 20:44:50 -0300

    Adding starting pages, home, login and register from user and adding initial setings on react routes

--- get_a_pet_source_code/frontend/src/App.js
+++ get_a_pet_source_code/frontend/src/App.js
@@ -1,8 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./components/pages/Home";
+import Login from "./components/pages/Auth/Login";
+import Register from "./components/pages/Auth/Register";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Get A Pet</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
